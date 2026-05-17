d54c00ee4ab65cc3d92562f6bcf3e8dfad2d9f0e
Roblox Indonesia <roblox.indonesia.id.com@gmail.com>
2026-05-17 09:55:51 +0700

    Add files via upload

---
+++ avatarlink.js
@@ -0,0 +1,72 @@
+function RI0v18(){
+document.getElementById("avatar").src="https://tr.rbxcdn.com/30DAY-Avatar-CB8589637A503A67AC049F85AA14D9EF-Png/720/720/Avatar/Webp/noFilter";
+document.getElementById("profil").src="https://tr.rbxcdn.com/30DAY-AvatarHeadshot-CB8589637A503A67AC049F85AA14D9EF-Png/150/150/AvatarHeadshot/Webp/noFilter";
+}
+function RI0v17(){
+document.getElementById("avatar").src="https://tr.rbxcdn.com/30DAY-Avatar-6595EE99A29A42CD75FD9A77D3B06A6E-Png/720/720/Avatar/Webp/noFilter";
+document.getElementById("profil").src="https://tr.rbxcdn.com/30DAY-AvatarHeadshot-6595EE99A29A42CD75FD9A77D3B06A6E-Png/150/150/AvatarHeadshot/Webp/noFilter";
+}
+function RI0v16(){
+document.getElementById("avatar").src="https://tr.rbxcdn.com/30DAY-Avatar-781DD2D76B08224282DFA19D6B540479-Png/720/720/Avatar/Webp/noFilter";
+document.getElementById("profil").src="https://tr.rbxcdn.com/30DAY-AvatarHeadshot-781DD2D76B08224282DFA19D6B540479-Png/150/150/AvatarHeadshot/Webp/noFilter";
+}
+function RI0v15(){
+document.getElementById("avatar").src="https://tr.rbxcdn.com/30DAY-Avatar-148B3D1BFEA6D5A9EA35614C9E07E1BC-Png/720/720/Avatar/Webp/noFilter";
+document.getElementById("profil").src="https://tr.rbxcdn.com/30DAY-AvatarHeadshot-148B3D1BFEA6D5A9EA35614C9E07E1BC-Png/150/150/AvatarHeadshot/Webp/noFilter";
+}
+function RI0v14(){
+document.getElementById("avatar").src="https://tr.rbxcdn.com/30DAY-Avatar-ED6E504DCD0989309333C3F87B84DC2E-Png/720/720/Avatar/Webp/noFilter";
+document.getElementById("profil").src="https://tr.rbxcdn.com/30DAY-AvatarHeadshot-ED6E504DCD0989309333C3F87B84DC2E-Png/150/150/AvatarHeadshot/Webp/noFilter";
+}
+function RI0v13(){
+document.getElementById("avatar").src="https://tr.rbxcdn.com/30DAY-Avatar-7D9968F61F1EAA55DBD237723F55A815-Png/720/720/Avatar/Webp/noFilter";
+document.getElementById("profil").src="https://tr.rbxcdn.com/30DAY-AvatarHeadshot-7D9968F61F1EAA55DBD237723F55A815-Png/150/150/AvatarHeadshot/Webp/noFilter";
+}
+function RI0v12(){
+document.getElementById("avatar").src="https://tr.rbxcdn.com/30DAY-Avatar-72ABCE95B88CB09C517C23CF50C633F0-Png/720/720/Avatar/Webp/noFilter";
+document.getElementById("profil").src="https://tr.rbxcdn.com/30DAY-AvatarHeadshot-72ABCE95B88CB09C517C23CF50C633F0-Png/150/150/AvatarHeadshot/Webp/noFilter";
+}
+function RI0v11(){
+document.getElementById("avatar").src="no-avatar.png";
+document.getElementById("profil").src="180DAY-a17918617b20ac9c39b305241f23e58a.png";
+}
+function RI0v10(){
+document.getElementById("avatar").src="no-avatar.png";
+document.getElementById("profil").src="180DAY-a17918617b20ac9c39b305241f23e58a.png";
+}
+function RI0v9(){
+document.getElementById("avatar").src="no-avatar.png";
+document.getElementById("profil").src="180DAY-a17918617b20ac9c39b305241f23e58a.png";
+}
+function RI0v8(){
+document.getElementById("avatar").src="no-avatar.png";
+document.getElementById("profil").src="180DAY-a17918617b20ac9c39b305241f23e58a.png";
+}
+function RI0v7(){
+document.getElementById("avatar").src="https://robloxindonesia.github.io/img/30DAY-Avatar-ED6E504DCD0989309333C3F87B84DC2E.png";
+document.getElementById("profil").src="https://RobloxIndonesia.github.io/img/30DAY-AvatarHeadshot-ED6E504DCD0989309333C3F87B84DC2E.png";
+}
+function RI0v6(){
+document.getElementById("avatar").src="no-avatar.png";
+document.getElementById("profil").src="180DAY-a17918617b20ac9c39b305241f23e58a.png";
+}
+function RI0v5(){
+document.getElementById("avatar").src="no-avatar.png";
+document.getElementById("profil").src="180DAY-a17918617b20ac9c39b305241f23e58a.png";
+}
+function RI0v4(){
+document.getElementById("avatar").src="no-avatar.png";
+document.getElementById("profil").src="180DAY-a17918617b20ac9c39b305241f23e58a.png";
+}
+function RI0v3(){
+document.getElementById("avatar").src="no-avatar.png";
+document.getElementById("profil").src="180DAY-a17918617b20ac9c39b305241f23e58a.png";
+}
+function RI0v2(){
+document.getElementById("avatar").src="no-avatar.png";
+document.getElementById("profil").src="180DAY-a17918617b20ac9c39b305241f23e58a.png";
+}
+function RI0v1(){
+document.getElementById("avatar").src="no-avatar.png";
+document.getElementById("profil").src="180DAY-a17918617b20ac9c39b305241f23e58a.png";
+}
