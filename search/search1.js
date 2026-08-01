6b958ec24336c006d82fc367baf3b50868fa7a98
Roblox Indonesia <robloxindonesia.old@gmail.com>
2026-08-01 22:13:42 +0700

    Add files via upload

---
+++ search/search1.js
@@ -0,0 +1,36 @@
+// robloxindonesia.js
+users.push({
+    avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-BF312426510122AC35984DAC6A25EBE1-Png/720/720/AvatarHeadshot/Webp/noFilter",
+    displayName: "Roblox_Indonesia",
+    username: "RobloxIndonesia_old",
+    certified: "/iconsvg/certified.svg",
+    plus: "/iconsvg/plus.svg",
+},
+{
+    avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-18A126B79B4008008A60A53EC49548DF-Png/150/150/AvatarHeadshot/Webp/noFilter",
+    displayName: "Roblox_Indo",
+    username: "azure7967",
+    certified: "/iconsvg/plus.svg",
+    plus: "/iconsvg/icon.svg",
+},
+{
+    avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-A82057A0F12A9ACBE002B6BBB27890ED-Png/150/150/AvatarHeadshot/Webp/noFilter",
+    displayName: "Deden_SuryaNegara",
+    username: "deden_sn",
+    certified: "/iconsvg/certified.svg",
+    plus: "/iconsvg/plus.svg",
+},
+{
+    avatar: "https://robloxindonesia.github.io/img/30DAY-AvatarHeadshot-ED6E504DCD0989309333C3F87B84DC2E.png",
+    displayName: "Roblox_Indonesia",
+    username: "RobIox_lndo",
+    certified: "/iconsvg/icon.svg",
+    plus: "/iconsvg/icon.svg",
+},
+{
+    avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-FEE8839CABCA89F28B014A5376478DCE-Png/150/150/AvatarHeadshot/Webp/noFilter",
+    displayName: "Admin_Indonesia",
+    username: "akudaf_6802",
+    certified: "/iconsvg/certified.svg",
+    plus: "/iconsvg/plus.svg",
+});
